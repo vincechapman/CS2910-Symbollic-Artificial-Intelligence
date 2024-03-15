1f54f1c40b3d6de938ccbba9a4d94e003366e435
Vince Chapman <vchapandrews@gmail.com>
2024-03-15 04:48:08 +0000

    Part 1 done

--- Coursework 2/cs2910CW2.pl
+++ Coursework 2/cs2910CW2.pl
@@ -12,29 +12,29 @@ leads_to(corridor, living_room).
 % Rule stating if two rooms are adjacent.
 adjacent_to(R1, R2) :- leads_to(R1, R2); leads_to(R2, R1).
 
-% List membership check
-is_member_of(X, [X|_]).
-is_member_of(X, [_|Tail]) :- is_member_of(X, Tail).
-
-print_head([Head], Reversed) :-
-    write(Reversed).
-print_head([Head|Tail], Reversed) :-
-    print_head(Tail, [Head|Reversed]).
-
+find_paths(Start, Goal, Solution) :-
+    df_search(Goal, [], Start, Solution). 
 
 % Recursive depth-first search
-df_search(_, Goal, _) :-
-    \+ adjacent_to(Goal, _),
-    write("Invalid goal given").
-df_search(_, _, Current) :-
-    \+ adjacent_to(Current, _),
-    write("Invalid start given").
-df_search(Visited, Goal, Current) :-
-    =(Goal, Current),
-    write(Visited),
-    X = [],
-    print_head(Visited, X).
-df_search(Visited, Goal, Current) :-
-    adjacent_to(Current, Adj),
-    \+ member(Adj, Visited),
-    df_search([Adj|Visited], Goal, Adj).
+df_search(Goal, Visited, Current, [Current | Visited]) :-
+    =(Goal, Current).
+df_search(Goal, Visited, Current, Solution) :-
+    adjacent_to(Current, Next),
+    \+ member(Next, Visited),
+    df_search(Goal, [Current | Visited], Next, Solution).
+
+% df_search(_, Goal, _) :-
+%     \+ adjacent_to(Goal, _),
+%     write("Invalid goal given").
+% df_search(_, _, Current) :-
+%     \+ adjacent_to(Current, _),
+%     write("Invalid start given").
+% df_search(Visited, Goal, Current) :-
+%     =(Goal, Current),
+%     write(Visited),
+%     X = [],
+%     print_head(Visited, X).
+% df_search(Visited, Goal, Current) :-
+%     adjacent_to(Current, Adj),
+%     \+ member(Adj, Visited),
+%     df_search([Adj|Visited], Goal, Adj).
