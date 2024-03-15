59baa30060bd7e0d07219a1c6ddbb51188e49da1
Vince Chapman <vchapandrews@gmail.com>
2024-03-15 05:48:30 +0000

    Add comments and better visual spacing

--- Coursework 2/cs2910CW2.pl
+++ Coursework 2/cs2910CW2.pl
@@ -13,28 +13,48 @@ leads_to(corridor, living_room).
 adjacent_to(R1, R2) :- leads_to(R1, R2); leads_to(R2, R1).
 
 
-% 3.1 Find a path
+
+
+% 3.1: Find a path
+% ---------------------------------------------------------
 
 find_paths(Start, _, _) :-
     \+ adjacent_to(Start, _), !,
     write("Invalid start room given for first argument.").
+
 find_paths(_, Goal, _) :-
     \+ adjacent_to(Goal, _), !,
     write("Invalid goal room given for second argument.").
+
 find_paths(_, _, Solution) :-
     \+ var(Solution), !,
     write("Third predicate argument must be a variable.").
+
 find_paths(Start, Goal, Solution) :-
     df_search(Goal, [], Start, X),
-    reverse(X, Solution, []).
+    reverse(X, [], Solution).
+
+
+% Depth-first search algorithm
 
 df_search(Goal, Visited, Current, [Current | Visited]) :-
     =(Goal, Current).
+
 df_search(Goal, Visited, Current, Solution) :-
     adjacent_to(Current, Next),
     \+ member(Next, Visited),
     df_search(Goal, [Current | Visited], Next, Solution).
 
-reverse([],Z,Z).
-reverse([H|T],Z,Acc) :-
-    reverse(T,Z,[H|Acc]).
+
+% Reverse list order algorithm
+
+reverse([], X, X). % Base case, if original list empty
+
+reverse([Head | Tail], Acc, X) :-
+    reverse(Tail, [Head | Acc], X).
+
+
+
+
+% 3.2 Paths ending at a common destination
+% ---------------------------------------------------------
